8cb310cb5705ca0f1424bae91c1aeea5e8ab81a0
PebiDev <peter.biedermann@inode.at>
2025-04-27 09:47:36 +0200

    added Equipment

---
+++ src/data/equipment/special_equipment_IS.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    category: "Special Equipment",
+    type: "gear",
+    name: "A-Pods",
+    heat: "*",
+    damage: "*",
+    minimal: "-",
+    range: "n/a",
+    ammo: "OS",
+    tons: 0.5,
+    critical: 1,
+  },
+];
