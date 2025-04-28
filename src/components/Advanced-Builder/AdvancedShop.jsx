75aa645c59de3849c2179347fa7180a48debf7c0
PebiDev <peter.biedermann@inode.at>
2025-04-28 11:29:18 +0200

    created equipment files, added AdvancedShop component

---
+++ src/components/Advanced-Builder/AdvancedShop.jsx
@@ -0,0 +1,12 @@
+import ShopItem from "../Builder/ShopItem";
+import { useSelector } from "react-redux";
+import autoCannons_IS from "../../data/equipment_IS/autocannons_IS";
+import flamers_IS from "../../data/equipment_IS/Flamers_IS";
+import gaussRifles_IS from "../../data/equipment_IS/GaussRifles_IS";
+import lasers_IS from "../../data/equipment_IS/Lasers_IS";
+
+const AdvancedShop = () => {
+  return <></>;
+};
+
+export default AdvancedShop;
--- src/components/Builder/ShopEquipment.jsx
+++ src/components/Builder/ShopEquipment.jsx
@@ -1,38 +1,42 @@
 import equipment_lvl1 from "../../data/equipment_lvl1";
 import { useSelector } from "react-redux";
-import autocannons_IS from "../../data/equipment_IS/autocannons_IS";
+import AdvancedShop from "../Advanced-Builder/AdvancedShop";
 
 import ShopItem from "./ShopItem";
 import ShopAmmo from "./ShopAmmo";
 
 const EQUIPMENT = equipment_lvl1;
-const ADVANCED_EQUIPMENT_IS = {};
-const ac_is = autocannons_IS;
 
 const ShopEquipment = () => {
   const mech = useSelector((state) => state.mech);
+  const ui = useSelector((state) => state.ui);
   return (
     <div id="shop-equipment" className="form-element">
       <ShopAmmo />
+
       <h3>Choose Weapons</h3>
-      <table id="shop-table">
-        <thead>
-          <tr>
-            <th>Weapon</th>
-            <th>Tons</th>
-            <th>Critical</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {EQUIPMENT.map((item) => {
-            //current solution: only display items that can still fit in the mech
-            if (item.critical <= mech.criticalSlots) {
-              return <ShopItem item={item} key={item.name} />;
-            }
-          })}
-        </tbody>
-      </table>
+      {ui.advancedOptions ? (
+        <AdvancedShop />
+      ) : (
+        <table id="shop-table">
+          <thead>
+            <tr>
+              <th>Weapon</th>
+              <th>Tons</th>
+              <th>Critical</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {EQUIPMENT.map((item) => {
+              //current solution: only display items that can still fit in the mech
+              if (item.critical <= mech.criticalSlots) {
+                return <ShopItem item={item} key={item.name} />;
+              }
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
